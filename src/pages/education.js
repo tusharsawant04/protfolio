a6739fbb8acdb59013851ec3d37fc3ac7e3b03f1
tusharsawant04 <tsawant379@gmail.com>
2023-11-28 09:56:55 +0530

    change

--- src/pages/education.js
+++ src/pages/education.js
@@ -61,16 +61,15 @@ class Education extends Component {
       <>
         <div className="container">
           <h2>Education Details</h2>
-          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3 ml-4">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 ml-4">
             {this.state.education.map((edu) => (
                <div className="col mb-4" key={edu.id}>
               <div className="card ml-4 d-flex align-items-center">
-                <img src={edu.image} className="card-img-top" alt={edu.degree} />
                 <div className="card-body">
                   <h3 className="card-title">{edu.degree}</h3>
-                  <p className="card-text">{edu.university}</p>
-                  <p className="card-text">{edu.year}</p>
-                  <p className="card-text">{edu.percentage}</p>
+                  <p className="card-text text-dark">{edu.university}</p>
+                  <p className="card-text text-dark">{edu.year}</p>
+                  <p className="card-text text-dark">{edu.percentage}</p>
                   </div>
                 </div>
               </div>
